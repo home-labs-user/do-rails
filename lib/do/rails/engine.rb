34f7709f25ff3a4b9c9f0d393528a54a418efe87
Rafael Laurindo <rafaelplaurindo@gmail.com>
2015-02-24 20:34:53 -0300

    fix bug

--- lib/do/rails/engine.rb
+++ lib/do/rails/engine.rb
@@ -2,13 +2,13 @@ module Do
   module Rails
 
     class Engine < ::Rails::Engine
-      isolate_namespace Do
+      # isolate_namespace Do
 
-      config.before_initialize do
+      # config.before_initialize do
         # << faz push no array paths com uma string informada
         #Dir["./"].each { |p| Rails.application.config.assets.paths << p }
         # Se for o caso, usar: root.join("lib/assets/javascripts")
-      end
+      # end
 
       # verificar se é necessário
       # ActiveSupport.on_load(:action_view) do
