5571d683545d5f947bf587f2a01229b12def658b
Rafael Laurindo <rafaelplaurindo@gmail.com>
2015-10-04 11:42:35 -0300

    remove comentário

--- lib/do/rails/engine.rb
+++ lib/do/rails/engine.rb
@@ -2,7 +2,6 @@ module Do
   module Rails
 
     class Engine < ::Rails::Engine
-      # isolate_namespace Do::Rails
 
       # config.before_initialize do
         # << faz push no array paths com uma string informada
