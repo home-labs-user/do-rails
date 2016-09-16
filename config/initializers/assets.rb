e34cbdd2f2bda8d3a813d7c5d65e9c3de7a67095
Rafael Laurindo <home-labs-user>
2016-09-16 18:45:43 -0300

    add configuration file

---
+++ config/initializers/assets.rb
@@ -0,0 +1,7 @@
+# See what's difference between .application and .application.egine, if there is a effective difference.
+# Rails.application.engine.config.assets.compile += ...
+
+# Configs to Production
+
+# precompilation on deploy. Basicaly, must be precompile the assets that contains "//= require_tree .", that mount all files in one.
+Rails.application.config.assets.precompile += %w( esphinx.css esphinx.js )
